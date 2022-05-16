b0c9be2a2b42a9bf8f3c7a54cdb88ddb5590cf64
An Tran <tran.binhan@gmail.com>
2022-05-16 21:18:56 +0800

    Update RealFlags version

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/antranapp/DebugPane", .branch("master")),
-        .package(url: "https://github.com/antranapp/RealFlags", .branch("main")),
+        .package(url: "https://github.com/immobiliare/RealFlags", .exact("1.3.1")),
     ],
     targets: [
         .target(
